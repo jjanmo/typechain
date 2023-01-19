06c8870b5fe750e2aa729b2efbd17021aebf48a0
jjanmo <jjanmo07@hanmail.net>
2023-01-20 04:09:29 +0900

    feat: configure ts-environment and add block class

--- ver2022/src/index.ts
+++ ver2022/src/index.ts
@@ -1,4 +1,25 @@
-import { init, exit } from 'myPackage';
+// import crypto from 'crypto';
+import crypto from 'crypto';
+interface BlockBase {
+  prevHash: string;
+  height: number;
+  data: string;
+  hash: string;
+}
+class Block implements BlockBase {
+  public hash: string;
 
-init({ url: 'www.google.com' });
-exit(10);
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+
+    return crypto.createHash('sha256').update(toHash).digest('hex');
+  }
+}
